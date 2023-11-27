a4da2a5879b8c4829251d2fec698f09b8d77658f
shobhan28 <shobhan28m@gmail.com>
2023-11-27 17:20:50 +0530

    first commit

---
+++ src/main/java/com/flatmateapp/FlatMateApplication.java
@@ -0,0 +1,13 @@
+package com.flatmateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatMateApplication.class, args);
+	}
+
+}
