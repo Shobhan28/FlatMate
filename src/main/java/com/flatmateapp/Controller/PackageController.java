68cae57cbbb0c0601bcae468a59916985fab2a6c
shobhan28 <shobhan28m@gmail.com>
2023-11-28 18:01:34 +0530

    Add endpoint for confirming payments using Stripe API

--- src/main/java/com/flatmateapp/Controller/PackageController.java
+++ src/main/java/com/flatmateapp/Controller/PackageController.java
@@ -1,8 +1,11 @@
 package com.flatmateapp.Controller;
 
 import com.flatmateapp.Entity.LegalPackage;
+import com.flatmateapp.Entity.PackagePayment;
 import com.flatmateapp.Repository.PackageRepository;
 import com.flatmateapp.Service.LegalPackageService;
+import com.flatmateapp.Payload.PaymentConfirmationRequest;
+import com.flatmateapp.Service.PaymentService;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
@@ -21,6 +24,8 @@ import java.util.Map;
 public class PackageController {
 
     @Autowired
+    private PaymentService paymentService;
+    @Autowired
     private LegalPackageService legalPackageService;
     @Autowired
     private PackageRepository packageRepository;
@@ -33,5 +38,79 @@ public class PackageController {
         return new ResponseEntity<>(legalPackages, HttpStatus.OK);
     }
 
+    @PostMapping("/purchase/{packageId}")
+    public ResponseEntity<String> purchaseLegalPackage(@PathVariable("packageId") Long productId) {
+        LegalPackage legalPackage = legalPackageService.getLegalProductById(productId);
 
+        if (legalPackage == null) {
+            return new ResponseEntity<>("Legal package not found", HttpStatus.NOT_FOUND);
+        }
+
+        // Create a Stripe payment intent here and get the client secret
+        String clientSecret;
+        try {
+            clientSecret = createPaymentIntent(legalPackage.getPrice());
+        } catch (StripeException e) {
+            return new ResponseEntity<>("Error creating payment intent", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        // Return the client secret to the client side
+        return new ResponseEntity<>(clientSecret, HttpStatus.OK);
+    }
+
+    @PostMapping("/confirm-payment")
+    public ResponseEntity<String> confirmPayment(@RequestBody PaymentConfirmationRequest request) {
+        String paymentIntentId = request.getPaymentIntentId();
+        double amount = request.getAmount();
+        String currency = request.getCurrency();
+
+
+        // Validate the payment using Stripe API
+        boolean paymentSuccessful;
+        try {
+            paymentSuccessful = validatePayment(paymentIntentId, amount, currency);
+        } catch (StripeException e) {
+            return new ResponseEntity<>("Error validating payment", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (paymentSuccessful) {
+            PackagePayment payment = new PackagePayment();
+            payment.setPaymentIntentId(paymentIntentId);
+            payment.setAmount(amount);
+            payment.setCurrency(currency);
+
+            paymentService.savePayment(payment);
+
+            return new ResponseEntity<>("Payment confirmed", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Payment failed", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    private String createPaymentIntent(double amount) throws StripeException {
+        Stripe.apiKey = stripeSecretKey;
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", (int) (amount * 100)); // Amount is in cents
+        params.put("currency", "usd"); // Replace with your desired currency
+        params.put("payment_method_types", "card");
+
+        PaymentIntent paymentIntent = PaymentIntent.create(params);
+
+        return paymentIntent.getClientSecret();
+    }
+
+    private boolean validatePayment(String paymentIntentId, double amount, String currency) throws StripeException {
+        Stripe.apiKey = stripeSecretKey;
+
+        PaymentIntent paymentIntent = PaymentIntent.retrieve(paymentIntentId);
+
+        // Check if the payment was successful
+        return paymentIntent.getStatus().equals("succeeded")
+                && paymentIntent.getAmountReceived() == (int) (amount * 100) // Amount is in cents
+                && paymentIntent.getCurrency().equalsIgnoreCase(currency);
+    }
+
+    // Stripe API integration methods (createPaymentIntent, validatePayment) go here
 }
