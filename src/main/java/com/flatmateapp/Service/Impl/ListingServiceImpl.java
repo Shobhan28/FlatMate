0889d535a9bc622f8b326da5aaa591a7ba1874c6
shobhan28 <shobhan28m@gmail.com>
2023-11-28 10:52:59 +0530

    overridden getListingById method and build logic

--- src/main/java/com/flatmateapp/Service/Impl/ListingServiceImpl.java
+++ src/main/java/com/flatmateapp/Service/Impl/ListingServiceImpl.java
@@ -25,8 +25,8 @@ public class ListingServiceImpl implements ListingService {
     @Override
     public ListingDTO saveListing(ListingDTO listingDTO) {
         Listing listingEntity = mapToEntity(listingDTO);
-       // Status will be by default pending,
-       // so whatever status you will give in json object, it doesnt matter.
+        // Status will be by default pending,
+        // so whatever status you will give in json object, it doesnt matter.
         listingEntity.setStatus("pending");
         Listing savedListing = listingRepository.save(listingEntity);
         return mapToDto(savedListing);
@@ -34,24 +34,44 @@ public class ListingServiceImpl implements ListingService {
 
     @Override
     public ListingDTO approvePropertyListing(Long listingId) {
-            Optional<Listing> optionalProperty = listingRepository.findById(listingId);
+        Optional<Listing> optionalProperty = listingRepository.findById(listingId);
 
-            if (optionalProperty.isPresent()) {
-                Listing listing = optionalProperty.get();
-                listing.setStatus("approved");
-                listing = listingRepository.save(listing);
+        if (optionalProperty.isPresent()) {
+            Listing listing = optionalProperty.get();
+            listing.setStatus("approved");
+            listing = listingRepository.save(listing);
+            return mapToDto(listing);
+        } else {
+            // Handle property not found
+            // You can throw an exception or return null/empty, depending on your design
+            return null;
+        }
+    }
+
+    @Override
+    public ListingDTO getListingById(Long listingId) {
+        Optional<Listing> optionalListing = listingRepository.findById(listingId);
+
+        if (optionalListing.isPresent()) {
+            Listing listing = optionalListing.get();
+            if ("approved".equals(listing.getStatus())) {
                 return mapToDto(listing);
             } else {
-                // Handle property not found
-                // You can throw an exception or return null/empty, depending on your design
-                return null;
+                // Handle case where status is not approved
+                throw new IllegalStateException("Listing with ID " + listingId + " is not approved.");
             }
+        } else {
+            // Handle case where listing with the given ID is not found
+            throw new IllegalArgumentException("Listing with ID " + listingId + " not found.");
+        }
     }
+
 
     private Listing mapToEntity(ListingDTO listingDto) {
         return modelMapper.map(listingDto, Listing.class);
 
-}
+    }
+
     ListingDTO mapToDto(Listing listing) {
         return modelMapper.map(listing, ListingDTO.class);
     }
