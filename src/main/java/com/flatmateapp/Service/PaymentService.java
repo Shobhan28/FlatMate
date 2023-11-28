613b19233637fb155542f49905db3ae0e6ba26c1
shobhan28 <shobhan28m@gmail.com>
2023-11-28 18:03:24 +0530

    "Defined savePayment method in PaymentService interface"

---
+++ src/main/java/com/flatmateapp/Service/PaymentService.java
@@ -0,0 +1,8 @@
+package com.flatmateapp.Service;
+
+import com.flatmateapp.Entity.PackagePayment;
+
+public interface PaymentService {
+    public PackagePayment savePayment(PackagePayment payment) ;
+
+}
